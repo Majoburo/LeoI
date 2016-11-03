b571a9d63a8e43d31f1e6f909a13aae3fd2af1b9
Maria Jose Bustamante Rosell <majoburo@login1.maverick.tacc.utexas.edu>
2016-11-03 17:13:52 -0500

    Launch script for the batch jobs.

---
+++ psf/mc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+iterations=1000
+sigmamin=5
+sigmamax=40
+sigmastep=0.25
+rm Leo_table.fits
+python psf.py 10 3
+rm std_10.00_3.txt
+python writebatch.py $iterations $sigmamin $sigmamax $sigmastep
+sbatch psfbatch.slurm
