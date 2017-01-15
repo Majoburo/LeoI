410c3abaf59a6c5798827da318d5b68b96f480f3
Robbie Rosati <rjrosati@utexas.edu>
2017-01-14 22:33:59 -0600

    MPI sequential writing should work, need to set initial conditions

--- psf/psf.py
+++ psf/psf.py
@@ -202,18 +202,24 @@ def calculatestd(t):
 
 if __name__ == '__main__':
     if os.path.isfile("Leo_table.hdf5"):
-        t = Table.read("Leo_table.hdf5")
-        #calculatestd(t)
-        #'''
-        astd = np.zeros((len(t), 1, 2))
-        astd[t['N_Stars'] > 0] = calculatestd(t)
-        ostd = np.array(t['Estimated_Std'])
-        #if len(ostd.shape)==2:
-        #    ostd.shape = (ostd.shape[0], 1, ostd.shape[1])
-        astd = np.concatenate((ostd, astd), axis=1)
-        t.replace_column('Estimated_Std', astd)
-        t.write("Leo_table.hdf5", path = 'updated_data', overwrite=True)
-        #'''
+        curr_rank=0
+        while(curr_rank<size):
+            if rank == curr_rank:
+                t = Table.read("Leo_table.hdf5")
+                #calculatestd(t)
+                #'''
+                astd = np.zeros((len(t), 1, 2))
+                astd[t['N_Stars'] > 0] = calculatestd(t)
+                ostd = np.array(t['Estimated_Std'])
+                #if len(ostd.shape)==2:
+                #    ostd.shape = (ostd.shape[0], 1, ostd.shape[1])
+                astd = np.concatenate((ostd, astd), axis=1)
+                t.replace_column('Estimated_Std', astd)
+                t.write("Leo_table.hdf5", path = 'updated_data', overwrite=True)
+                #'''
+            if rank == 0:
+                curr_rank+=1
+            curr_rank = comm.bcast(curr_rank,root=0)
     else:
         #Loading vw position an kinematic data:
         vwpos = VW_IFU_pos(config["vw"]["regionsfile"])
